b5c772a24e0d826c1675f692442b53629407e574
Steve XU <stevexu4@gmail.com>
2021-02-10 18:42:37 +0100

    Added fixed collision
    Blocks won't go through each other

--- Assets/Scripts/FallPhysics.cs
+++ Assets/Scripts/FallPhysics.cs
@@ -4,59 +4,69 @@ using UnityEngine;
 
 public class FallPhysics : MonoBehaviour
 {
-    private Rigidbody2D rb; 
-    private bool asleep;
-
-    public float fallTime = 0.0f;
-    public float fallSpeed = 1;
+    private Rigidbody2D rb;   
+    public Vector2 movement;
+    public float fallSpeed = 6.0f;
+    public bool collide;
     
     // Start is called before the first frame update
     void Start()
     { 
-        rb = gameObject.GetComponent<Rigidbody2D>();   
-        asleep = true;
+        this.rb = gameObject.GetComponent<Rigidbody2D>();    
+        this.rb.velocity = new Vector2(0, -2.0f);
+        
+        collide = false;
     }
 
     // Update is called once per frame
     void Update()
-    {       
-        if(asleep)
+    {           
+        if(collide == false)
             CheckUserInput(); 
-    }
+        
+        if(gameObject.transform.position.y<3)
+        {
+            Destroy(gameObject);
+            Destroy(this.rb);
+            Destroy(this);
+        }
+    } 
 
     //
     void CheckUserInput()
-    {
-        if (Input.GetKeyDown("s") || Time.time - fallTime >= fallSpeed)
+    { 
+        if(Input.GetKey("s"))
         {
-            gameObject.transform.position += new Vector3(0, -1, 0);
-            fallTime = Time.time;
+            this.rb.velocity = new Vector2(0, -6.0f);
         }
-        else if(Input.GetKeyDown("q"))
+        else 
         {
-            gameObject.transform.position += new Vector3(-1, 0, 0);
-        }
-        else if(Input.GetKeyDown("d"))
-        {
-            gameObject.transform.position += new Vector3(1, 0, 0);
-        }
-        else if(Input.GetKeyDown("a"))
-        {
-            gameObject.transform.Rotate(0, 0, 90);
-        }
-        else if(Input.GetKeyDown("e"))
-        {
-            gameObject.transform.Rotate(0, 0, -90);
+            this.rb.velocity = new Vector2(0, -2.0f);
+            if(Input.GetKeyDown("q"))
+            {
+                gameObject.transform.position += new Vector3(-1, 0, 0);
+            }
+            else if(Input.GetKeyDown("d"))
+            {
+                gameObject.transform.position += new Vector3(1, 0, 0);
+            }
+            else if(Input.GetKeyDown("a"))
+            {
+                gameObject.transform.Rotate(0, 0, 90);
+            }
+            else if(Input.GetKeyDown("e"))
+            {
+                gameObject.transform.Rotate(0, 0, -90); 
+            } 
         }
     }
 
+    //
     void OnCollisionEnter2D(Collision2D c)
     {
         if(c.gameObject.tag == "Block" || c.gameObject.tag == "Ground")
-        {
-            rb.gravityScale = 1.0f;
-            asleep = false;
-            rb.WakeUp();
+        { 
+            collide = true;
         }
     }
 }

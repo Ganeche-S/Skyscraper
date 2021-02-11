4a49eac6557f9cd43f11258db585072d3936551d
Steve XU <stevexu4@gmail.com>
2021-02-11 17:53:35 +0100

    Added Start menu and spawner

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    private Rigidbody2D rb;   
+    private bool collide;   
+    
+    // Start is called before the first frame update
+    void Start()
+    { 
+        this.rb = gameObject.GetComponent<Rigidbody2D>();    
+        this.rb.velocity = new Vector2(0, -2.0f);
+        
+        this.collide = false;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {            
+        if(!collide)
+            CheckUserInput(); 
+
+        if(gameObject != null && gameObject.transform.position.y<3)
+        {
+            Destroy(gameObject);
+            Destroy(this.rb);
+            Destroy(this);
+        }
+    } 
+
+    //
+    void CheckUserInput()
+    { 
+        if(Input.GetKey("s"))
+        {
+            this.rb.velocity = new Vector2(0, -6.0f);
+        }
+        else 
+        {
+            this.rb.velocity = new Vector2(0, -2.0f);
+            if(Input.GetKeyDown("q"))
+            {
+                gameObject.transform.position += new Vector3(-1, 0, 0);
+            }
+            else if(Input.GetKeyDown("d"))
+            {
+                gameObject.transform.position += new Vector3(1, 0, 0);
+            }
+            else if(Input.GetKeyDown("a"))
+            {
+                gameObject.transform.Rotate(0, 0, 90);
+            }
+            else if(Input.GetKeyDown("e"))
+            {
+                gameObject.transform.Rotate(0, 0, -90); 
+            } 
+        }
+    }
+
+    public bool HasCollide() 
+    {
+        return this.collide;
+    }
+ 
+    //
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        if(c.gameObject.tag == "Block" || c.gameObject.tag == "Ground")
+        { 
+            collide = true;
+        }
+    }
+}
--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,18 +1,103 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine; 
 
 public class Game : MonoBehaviour
 {
+    public GameObject[] Blocks; 
+    public GameObject currentBlock;  
+    // public int score;
+ 
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   
+        // score = 0; 
+
+        SpawnBlock(); // Initiate the first block to start the "loop" 
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        if(!EndGame())
+        {
+            Blocks = GameObject.FindGameObjectsWithTag("Block");
+            if(currentBlock.GetComponent<Block>().HasCollide())
+            {
+                SpawnBlock();
+            }
+        }
+        // score = CountPlayerScore(); 
+    }  
+    
+    // public int CountPlayerScore()
+    // {
+    //     int s = 0;
+    //     int length = GameObject.FindGameObjectsWithTag("Block").Length;
+    //     for(int i=0; i<length/10; i++)
+    //     {
+    //         if(i == (length/10)-1)
+    //             s += (i+1)*(length%10);
+    //         else
+    //             s += (i+1)*10;
+    //     }
+
+    //     return s;
+    // }
+
+    public bool EndGame()
+    {
+        bool collision = false;
+        foreach(GameObject go in Blocks)
+        {
+            collision = go.GetComponent<Block>().HasCollide();
+            if(collision && go.transform.position.y>=18) 
+            {
+                return true;
+            }
+        }
+        return false;
     }
+
+    public void SpawnBlock()
+    {
+        Vector3 position = new Vector3(18, 19, 0);
+        currentBlock = (GameObject) Instantiate(Resources.Load<GameObject>(GetRandomBlock()), position, Quaternion.identity);
+    
+    }
+
+    public string GetRandomBlock()
+    {
+        int random = Random.Range(1,8);
+
+        string randomBlock = "Prefabs/";
+
+        switch(random)
+        {
+            case 1:
+                randomBlock += "Block_I";
+                break;
+            case 2:
+                randomBlock += "Block_O";
+                break;
+            case 3:
+                randomBlock += "Block_LL";
+                break;
+            case 4:
+                randomBlock += "Block_LR";
+                break;
+            case 5:
+                randomBlock += "Block_S";
+                break;
+            case 6:
+                randomBlock += "Block_Z";
+                break;
+            case 7:
+                randomBlock += "Block_T";
+                break;
+        }
+        Debug.Log(randomBlock);
+
+        return randomBlock;
+    }
+     
 }

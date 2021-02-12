a17431deb6c63599883d0f08d3826f87bf8431ef
Ryan-Coral <56357340+Ryan-Coral@users.noreply.github.com>
2021-02-12 14:21:35 +0100

    musicBG

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 { 
+	
     public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // Load scene[1] = Game
